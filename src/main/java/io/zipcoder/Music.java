bc4ee4f1c23425626ab44d3024306a341f770f51
kendrang <kendrang@users.noreply.github.com>
2019-10-25 13:01:14 -0400

    done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,30 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int forwardsCounter = 0;
+        int backwardsCounter = 0;
+        for (int i = 0; i < startIndex + 1; i++) {
+            forwardsCounter++;
+            if (playList[i].equals(selection)) {
+                break;
+            }
+        }
+        for (int j = startIndex; j < playList.length; j++) {
+            backwardsCounter++;
+            if (playList[j].equals(selection)) {
+                break;
+            }
+        }
+        if (forwardsCounter > backwardsCounter) {
+            return backwardsCounter;
+        } else {
+            return forwardsCounter;
+        }
     }
+
 }
